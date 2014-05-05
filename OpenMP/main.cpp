e017381142a23949ec52bcde3a1fdb19a5e44feb
MacUser <home@air-anton.mshome.net>
2014-05-06 00:58:33 +0300

    Initial Commit

---
+++ OpenMP/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenMP
+//
+//  Created by MacUser on 06.05.14.
+//  Copyright (c) 2014 MacUser. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
